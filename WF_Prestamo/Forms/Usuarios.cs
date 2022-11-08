1490a932536d9e8185423e6362646dad7526cb37
ianhein <ianhein50@gmail.com>
2022-11-08 15:44:52 -0300

    fix

--- WF_Prestamo/Forms/Usuarios.cs
+++ WF_Prestamo/Forms/Usuarios.cs
@@ -12,6 +12,7 @@ using WF_Prestamo.Persistencia;
 using WF_Prestamo.Forms;
 using WF_Prestamo.Forms.Ubicaciones;
 using WF_Prestamo.Forms.Equipos;
+using WF_Prestamo.Forms.Préstamos;
 
 namespace WF_Prestamo.Forms
 {
@@ -92,7 +93,8 @@ namespace WF_Prestamo.Forms
         private void listarPréstamosToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Conexion.OpenConexion();
-            fmPrestamo
+            fmPrestamo p = new fmPrestamo();
+            p.ShowDialog();
         }
     }
 }
